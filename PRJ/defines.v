244b5c47b034c303e74fa86f239bd5509e3776f3
nomoregood <z13782850319@outlook.com>
2021-08-30 20:41:26 +0800

    define edit

--- PRJ/defines.v
+++ PRJ/defines.v
@@ -35,5 +35,13 @@
 `define InstMemNumLog2              17              
 
 
-
+//====================与通用寄存器Regfile有关的宏定义=============//
+`define RegAddrBus                  4:0
+`define RegBus                      31:0
+`define RegWidth                    32
+`define DoubleRegWidth              64
+`define DoubleRegBus                63:0
+`define RegNum                      32
+`define RegNumLog2                  5
+`define NOPRegAddr                  5'b00000
 
